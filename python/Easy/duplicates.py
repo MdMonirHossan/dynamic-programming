fe156ab297f697437f80afb455370304ed90eb4e
MdMonirHossan <mdraju01820@gmail.com>
2024-08-07 10:57:43 +0600

    Refactor

---
+++ python/Easy/duplicates.py
@@ -0,0 +1,32 @@
+'''
+	Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+	
+	Example 1:
+	Input: nums = [1,2,3,1]
+	Output: true
+	
+	Example 2:
+	Input: nums = [1,2,3,4]
+	Output: false
+	
+	Example 3:
+	Input: nums = [1,1,1,3,3,4,3,2,4,2]
+	Output: true
+'''
+def duplicate(nums):
+
+	hash_set = set()
+	count = {}
+	for n in nums:
+		# count[n] = 1 + count.get(count[n], 0)
+		# if count[n] > 1:
+		# 	return True
+		if n in hash_set:
+			return True
+		hash_set.add(n)
+	return False
+
+
+nums = [1,2,3,1]
+
+print(duplicate(nums))
--- typescript/Medium/group_anagrams.ts
+++ typescript/Medium/group_anagrams.ts
@@ -11,12 +11,12 @@ class GroupAnagrams {
     for (let i in this.strings) {
       let count: number[] = Array(26).fill(0);
       for (let j in this.strings[i].split("")) {
-        count[this.strings[i].charCodeAt(j) - "a".charCodeAt(0)] += 1;
+        count[this.strings[i].charCodeAt(Number(j)) - "a".charCodeAt(0)] += 1;
       }
       result[String(count)] = result[String(count)] || [];
       result[String(count)].push(this.strings[i]);
     }
-    return Object.keys(result).map(key => result[key])
+    return Object.keys(result).map((key) => result[key]);
   }
 }
 
