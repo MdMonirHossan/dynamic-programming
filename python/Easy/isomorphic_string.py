c197aebfc3e783b9f3e72aa9e1c6eb05c67a138e
MdMonirHossan <mdraju01820@gmail.com>
2024-08-08 18:45:14 +0600

    refactor

--- python/Easy/isomorphic_string.py
+++ python/Easy/isomorphic_string.py
@@ -1,10 +1,28 @@
-class Isophormic:
+'''
+	Given two strings s and t, determine if they are isomorphic.
+	Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+	All occurrences of a character must be replaced with another character while preserving the order of characters. 
+	No two characters may map to the same character, but a character may map to itself.
+
+	Example 1:
+	Input: s = "egg", t = "add"
+	Output: true
+	
+	Example 2:
+	Input: s = "foo", t = "bar"
+	Output: false
+	
+	Example 3:
+	Input: s = "paper", t = "title"
+	Output: true
+'''
+class Isomorphic:
 
 	def __init__(self, str1, str2):
 		self.str1 = str1
 		self.str2 = str2
 
-	def is_isophormic(self):
+	def is_isomorphic(self):
 
 		if len(self.str1) != len(self.str2):
 			return False
@@ -32,6 +50,6 @@ class Isophormic:
 
 if __name__ == '__main__':
 
-	obj = Isophormic('abacd', 'title')
+	obj = Isomorphic('abacd', 'title')
 
-	print(obj.is_isophormic())
+	print(obj.is_isomorphic())
