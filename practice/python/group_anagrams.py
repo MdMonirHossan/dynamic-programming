4b3c6fd1ffcd6df930c6405001a81fa269cc1019
MdMonirHossan <mdraju01820@gmail.com>
2025-05-20 22:41:16 +0600

    Practice groups anagrams problem in py

--- practice/python/group_anagrams.py
+++ practice/python/group_anagrams.py
@@ -24,41 +24,19 @@ Constraints:
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters.
 '''
-
 from collections import defaultdict
-
 class GroupAnagrams:
     def __init__(self, strings):
         self.strings = strings
-    # O(m*n) time complexity
-    def anagrams(self):
+    
+    def solution(self):
         result = defaultdict(list)
-        
         for string in self.strings:
-            count = [0] * 26  # 26 character [0,0,0,0,0,....]
+            count = [0] * 26
             for c in string:
                 count[ord(c) - ord('a')] += 1
             result[tuple(count)].append(string)
         return list(result.values())
-        
-    def anagrams_2(self):
-        if len(self.strings) == 0:
-            return self.strings
-        elif len(self.strings) == 1 and self.strings[0] == "":
-            return self.strings
-        result = {}
-        groups = []
-        for string in self.strings:
-            sort_str = ''.join(sorted(string))
-            print(sort_str)
-            if sort_str not in result:
-                result[sort_str] = [tuple([sort_str, string])]
-            else:
-                result[sort_str].append(tuple([sort_str, string]))
-        for key, value in result.items():
-            anagrams = [item[1] for item in value]
-            groups.append(anagrams)
-        print(groups)
 
-anagramsObj = GroupAnagrams(["act","pots","tops","cat","stop","hat"])
-print(anagramsObj.anagrams_2())
+group_anagrams_obj = GroupAnagrams(['cat', 'atc', 'bat', 'tab'])
+print(group_anagrams_obj.solution())
