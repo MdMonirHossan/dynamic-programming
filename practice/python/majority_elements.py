2b0c2ca495b69acb11298f65932c3b1563b4031f
MdMonirHossan <mdraju01820@gmail.com>
2025-05-24 12:35:45 +0600

    Practice majority elements in an array in py

--- practice/python/majority_elements.py
+++ practice/python/majority_elements.py
@@ -19,26 +19,18 @@ n == nums.length
 
 Follow-up: Could you solve the problem in linear time and in O(1) space?
 '''
+class MajorityElements:
+    def __init__(self, arr):
+        self.arr = arr
 
-def majority_element(nums):
-	res, count = 0, 0
-
-	for n in nums:
-		if count == 0:
-			res = n
-		count += (1 if res == n else -1)
-	return res
-
-
-	# count = {}
-	# res, maxCount = 0, 0
-	# for n in nums:
-	# 	count[n] = 1 + count.get(n, 0)
-	# 	res = n if count[n] > maxCount else res
-	# 	maxCount = max(count[n], maxCount)
-	# return res 
+    def solution(self):
+        result, count = 0, 0
+        for n in self.arr:
+            if count == 0:
+                result = n
+            count += (1 if result == n else -1)
+        return result
 
 
-nums = [1, 2, 1, 1, 2, 1]
-
-print(majority_element(nums))
+majority_element_obj = MajorityElements([1, 2, 3, 1, 2, 2])
+print(majority_element_obj.solution())
